dab2d771bba69803c78369e41d8ff3d822390a24
aseel-sh <aseel.sh52@gmail.com>
2024-07-06 16:05:45 +0300

    fixing

--- Program.cs
+++ Program.cs
@@ -7,65 +7,26 @@ public class Program
     public static EmployeeRepo Repo = new EmployeeRepo("data source=.;Initial Catalog=Company;Integrated Security=True;TrustServerCertificate=True");
     public static void Main(string[] args)
     {
-        List<Employee> employees = new List<Employee>();
-        employees = GetEmployeeFromDb();
-
-        foreach (var employee in employees)
-        {
-            Console.WriteLine($"Id = {employee.Id} , Name = {employee.Name} , Monthly Salary = {employee.MonthlySalary} , DepartementId = {employee.DepartmentId}");
-        }
-
-        /*List<Department> departments = new List<Department>();
-        departments = GetDepartmentFromDB();*/
-
-
+/*        Console.WriteLine("-----------------------------------------------------------------------------");
+        Repo.GetEmployeeFromDb();
+        Console.WriteLine("-----------------------------------------------------------------------------");*/
+         
         Console.WriteLine("Third highest salary: ");
-        GetThirdHighestSalary();
+        Repo.GetThirdHighestSal();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
 
         Console.WriteLine("Employees with Yearly salary greater than 5000: ");
-        //GetYearlySalaryGreater();
+        Repo.GetYearlySalaryGreater();
 
+        Console.WriteLine("-----------------------------------------------------------------------------");
 
         Console.WriteLine("Employees whose name start with 'a': ");
-        //GetNamestartA();
+        Repo.GetNamestartA();
+
+        Console.WriteLine("-----------------------------------------------------------------------------");
     }
 
 
-    void GetNamestartA()
-    {
-        throw new NotImplementedException();
-    }
 
-    void GetYearlySalaryGreater()
-    {
-        throw new NotImplementedException();
-    }
-    void GetThirdHighestSalary()
-    {
-        var query = "SELECT TOP 1 * FROM (SELECT TOP 3 * FROM Employees ORDER BY MonthlySalary DESC) RESULT ORDER BY MonthlySalary";
-    }
-
-    private static List<Employee> GetEmployeeFromDb()
-    {
-        List<Employee> employees = new List<Employee>();
-        string connectionString = "data source=.;Initial Catalog=Company;Integrated Security=True;TrustServerCertificate=True";
-        SqlConnection connection = new SqlConnection(connectionString);
-        var query = "Select * from employees";
-        SqlCommand command = new SqlCommand(query, connection);
-        connection.Open();
-        var result = command.ExecuteReader();
-        while (result.Read())
-        {
-            employees.Add(new Employee
-            {
-                Id = result.GetInt32(0),
-                Name = result.GetString(1),
-                MonthlySalary = result.GetDecimal(2),
-                DepartmentId = result.GetInt32(3),
-            });
-        }
-        connection.Close();
-
-        return employees;
-    }
 }
--- Repo/EmployeeRepo.cs
+++ Repo/EmployeeRepo.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Company.GetData;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +18,72 @@ namespace Company.Repo
         {
             connection = new SqlConnection(connectionString);
             command = new SqlCommand(null, connection);
+        }
 
+        public void GetThirdHighestSal()
+        {
+            connection.Open();
+            var query = "SELECT TOP 1 * FROM (SELECT TOP 3 * FROM Employees ORDER BY MonthlySalary DESC) RESULT ORDER BY MonthlySalary";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader result = command.ExecuteReader();
 
+            PrintEmployeeData(result);
+
+            connection.Close();
+        }
+
+        public void GetYearlySalaryGreater()
+        {
+            connection.Open();
+            var query = "SELECT * FROM Employees WHERE MonthlySalary * 12 > 5000";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader result = command.ExecuteReader();
+
+            PrintEmployeeData(result);
+
+            connection.Close();
 
         }
 
+        public void GetNamestartA()
+        {
+            connection.Open();
+            var query = "SELECT * FROM Employees WHERE Name LIKE 'a%' ";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader result = command.ExecuteReader();
 
+            PrintEmployeeData(result);
+
+            connection.Close();
+        }
+
+        public void GetEmployeeFromDb()
+        {
+            connection.Open();
+            var query = "Select * from employees";
+            SqlCommand command = new SqlCommand(query, connection);
+            var result = command.ExecuteReader();
+
+            PrintEmployeeData(result);
+
+        }
+
+        private void PrintEmployeeData(SqlDataReader result)
+        {
+            while (result.Read())
+            {
+                var Id = result.GetInt32(0);
+                var Name = result.GetString(1);
+                var MonthlySalary = result.GetDecimal(2);
+                var DepartmentId = result.GetInt32(3);
+
+                Console.WriteLine("Id = {0} , Name = {1} , Monthly Salary = {2} , Dept Id = {3}", Id, Name, MonthlySalary, DepartmentId);
+            }
+        }
+
+        private void ExcecuteQuery(string query)
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+        }
     }
 }
